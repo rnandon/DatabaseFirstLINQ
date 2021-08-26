e37eadd359c7254bec8cc7350715b43ae96e3227
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-26 14:01:15 -0500

    completed problem 16

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -28,10 +28,10 @@ namespace DatabaseFirstLINQ
             ProblemTen();
             //ProblemEleven();
             //ProblemTwelve();
-            ProblemThirteen();
-            ProblemFourteen();
+            //ProblemThirteen();
+            //ProblemFourteen();
             //ProblemFifteen();
-            //ProblemSixteen();
+            ProblemSixteen();
             //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
@@ -238,7 +238,10 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
-
+            var product = _context.Products.Where(p => p.Name == "LG TV").SingleOrDefault();
+            product.Price = 155m;
+            _context.Products.Update(product);
+            _context.SaveChanges();
         }
 
         private void ProblemSeventeen()
