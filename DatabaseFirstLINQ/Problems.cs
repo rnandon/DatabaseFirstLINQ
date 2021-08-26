8fe8fbd74d91031f0c6dbd8935bc83e3d210a571
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-26 13:42:56 -0500

    completed problem 12

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -26,8 +26,8 @@ namespace DatabaseFirstLINQ
             ProblemEight();
             ProblemNine();
             ProblemTen();
-            //ProblemEleven();
-            //ProblemTwelve();
+            // ProblemEleven();
+            ProblemTwelve();
             //ProblemThirteen();
             //ProblemFourteen();
             //ProblemFifteen();
@@ -183,6 +183,14 @@ namespace DatabaseFirstLINQ
         private void ProblemTwelve()
         {
             // Create a new Product object and add that product to the Products table using LINQ.
+            Product newProduct = new Product()
+            {
+                Name = "LG TV",
+                Description = "Led tv in 4K.",
+                Price = 200.00m,
+            };
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
 
         }
 
